35141a9898e9d02218cf440cd2caf5086ab4eb5d
Trifun Tomevski <tomevskitrifun@gmail.com>
2024-04-23 13:27:32 +0200

    added portfolio

---
+++ src/content_option.js
@@ -0,0 +1,116 @@
+const logotext = "JuniorDev";
+const meta = {
+    title: "Trifun Tomevski",
+};
+
+const introdata = {
+    title: "I’m Trifun Tomevski",
+    animated: {
+        first: "I love coding",
+        second: "React Developer",
+        third: "I develop things",
+    },
+    description:
+        "Driven Front End Developer on the lookout for exciting opportunities to leverage my expertise in HTML, CSS, and JavaScript. Proficient in frameworks like Bootstrap and React, I thrive in creating dynamic and responsive web applications that seamlessly merge aesthetics with functionality. Dive into my portfolio to witness firsthand how I blend cutting-edge technology with intuitive design to deliver captivating user experiences.",
+    myImageUrl:
+        "https://images.unsplash.com/photo-1712219046742-11d308bd94fe?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+};
+
+const dataabout = {
+    title: "About me",
+    aboutme:
+        "Aspiring student and frontend developer with a love for learning and coding. My hobbies include sports and reading books. I identify myself as a disciplined person with the ability to solve problems quickly.",
+};
+const worktimeline = [
+    {
+        jobtitle: "LAN Engineer",
+        where: "Euro Games Technology",
+        date: "2023-2024",
+    },
+];
+
+const skills = [
+    {
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "JavaScript",
+        value: 60,
+    },
+    {
+        name: "Bootstrap",
+        value: 85,
+    },
+];
+
+const studied = [
+    {
+        title: "Brainster",
+        description: "Front End Development 2022-2023",
+    },
+    {
+        title: "Euro College",
+        description: "Bussiness and Administration 2019-2022",
+    },
+    {
+        title: "Udemy",
+        description: "React NextJs Udemy Courses 2023-2024",
+    },
+];
+
+const dataportfolio = [
+    {
+        img: "https://images.pexels.com/photos/22136977/pexels-photo-22136977.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description: "",
+        link: "https://github.com/TrifunTomevski/Simple-project-with-HTML-Bootstrap-JavaScript",
+    },
+    {
+        img: "https://images.pexels.com/photos/22391435/pexels-photo-22391435.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description:
+            "",
+        link: "https://brainsterstreetartists.netlify.app",
+    },
+    {
+        img: "https://images.pexels.com/photos/22391965/pexels-photo-22391965.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        description:
+            "",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    myEmail: "tomevskitrifun@gmail.com",
+    myPhone: "(+389)75204819",
+    description:
+        "Thank you for taking the time to visit my portfolio! I'm excited to connect with you. Feel free to reach out using the form below, and I'll get back to you as soon as possible. Your message is important to me, and I'm committed to responding swiftly to all inquiries. Looking forward to hearing from you! ",
+    serviceId: "service_id",
+    templateId: "template_id",
+    userId: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/TrifunTomevski",
+    facebook: "https://www.facebook.com/trifun.asdasd/",
+    linkedin: "https://www.linkedin.com/in/trifun-tomevski-46b56a290/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    studied,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
